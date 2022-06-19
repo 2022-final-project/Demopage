1fe919576a36311160e9d55fcfe9dff444815641
Jinseo Song <82093280+rxb8k@users.noreply.github.com>
2022-06-20 06:07:32 +0900

    fix: back-end complete, Model 실행 안 됨

--- demopage/QueryOptimizer/models.py
+++ demopage/QueryOptimizer/models.py
@@ -3,7 +3,12 @@ from django.db import models
 # Create your models here.
 class Query(models.Model):
     query_sentence=models.CharField(max_length=1000)
-    random_page_cost=models.FloatField(default=4.0)
+    random_page_cost_1=models.BooleanField(default=0)
+    random_page_cost_2=models.BooleanField(default=0)
+    random_page_cost_4=models.BooleanField(default=0)
+    random_page_cost_8=models.BooleanField(default=0)
+    random_page_cost_16=models.BooleanField(default=0)
+    random_page_cost_32=models.BooleanField(default=0)
 
     def __str__(self):
         return self.goal
--- demopage/QueryOptimizer/views.py
+++ demopage/QueryOptimizer/views.py
@@ -14,6 +14,24 @@ def enter_query(request):
 
 def predict(request, id):
     query_input=get_object_or_404(Query, pk=id)
-    result_cost=test_sentences(query_input)
+    result_cost=test_sentences(query_input).eval() # return type : [0, 0, 0, 0, 0, 0]
+
+    if result_cost[0]==1: query_input.random_page_cost_1=1
+    if result_cost[1]==1: query_input.random_page_cost_2=1
+    if result_cost[2]==1: query_input.random_page_cost_4=1
+    if result_cost[3]==1: query_input.random_page_cost_8=1
+    if result_cost[4]==1: query_input.random_page_cost_16=1
+    if result_cost[5]==1: query_input.random_page_cost_32=1
     query_input.save()
-    return render(request, 'result.html', {'random_page_cost':result_cost})
+
+    result={
+        'query': query_input.query_sentence,
+        'random_page_cost_1':query_input.random_page_cost_1,
+        'random_page_cost_2':query_input.random_page_cost_2,
+        'random_page_cost_4':query_input.random_page_cost_4,
+        'random_page_cost_8':query_input.random_page_cost_8,
+        'random_page_cost_16':query_input.random_page_cost_16,
+        'random_page_cost_32':query_input.random_page_cost_32
+    }
+
+    return render(request, 'result.html', result)
