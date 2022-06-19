061ae77dd943089e47218a78656ad00366a4071c
Jinseo Song <82093280+rxb8k@users.noreply.github.com>
2022-06-20 04:50:37 +0900

    add: save pretrained tokenizer and model

--- demopage/QueryOptimizer/apps.py
+++ demopage/QueryOptimizer/apps.py
@@ -1,12 +1,10 @@
 from django.apps import AppConfig
-from transformers import pipeline, BertTokenizer, BertForSequenceClassification
+from transformers import pipeline, BertTokenizer, BertForSequenceClassification, AutoTokenizer
 
-# tokenizer = BertTokenizer.from_pretrained("/Users/jinseo/Demopage/demopage/QueryOptimizer/pretrained_model/vocab.txt")
-    
 class QueryoptimizerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'QueryOptimizer'
-    predict_model=BertForSequenceClassification.from_pretrained('/Users/jinseo/Demopage/demopage/QueryOptimizer/pretrained_model'
-    )
+    predict_model=BertForSequenceClassification.from_pretrained('/Users/jinseo/Demopage/demopage/QueryOptimizer/pretrained_model'),
+    tokenizer = AutoTokenizer.from_pretrained('/Users/jinseo/Demopage/demopage/QueryOptimizer/pretrained_model')
 
 # /Users/jinseo/miniforge3/envs/ftvenv/bin/python manage.py runserver
