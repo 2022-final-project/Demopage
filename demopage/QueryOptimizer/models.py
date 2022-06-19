66731f05abd2aff537a4807f5d367f7e3ba4f303
Jinseo Song <82093280+rxb8k@users.noreply.github.com>
2022-06-20 04:07:00 +0900

    add: add Query Optimizer AI model and initial Model, View, URL, Template fix: fix gitignore

--- demopage/QueryOptimizer/models.py
+++ demopage/QueryOptimizer/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Query(models.Model):
+    query_sentence=models.CharField()
+    random_page_cost=models.FloatField(default=4.0)
+    # t
+
+    def __str__(self):
+        return self.goal
--- demopage/QueryOptimizer/views.py
+++ demopage/QueryOptimizer/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from cv2 import idct
+from django.shortcuts import get_object_or_404, render, redirect
+from .models import *
+from predict import test_sentences
 
 # Create your views here.
+
+def enter_query(request):
+    query_input=Query()
+    if request.method=='POST':
+        query_input.query_sentence=request.POST['queryInput']
+        query_input.save()
+        return redirect('main')
+    return render('main.html')
+
+def predict(request, id):
+    query_input=get_object_or_404(Query, pk=id)
+    result_cost=test_sentences(query_input)
+    query_input.save()
+    return render(request, 'result.html', {'random_page_cost':result_cost})
